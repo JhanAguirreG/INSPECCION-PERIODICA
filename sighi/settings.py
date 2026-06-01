4f828aeb154c979d19819065be5e69f6bceca5ef
jhan aguirre <jhan.aguirreg@gmail.com>
2026-06-01 09:36:11 -0500

    second commit

--- sighi/settings.py
+++ sighi/settings.py
@@ -7,11 +7,13 @@ import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cambia-esto-en-produccion'
+SECRET_KEY = 'jhanprueba'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    '.railway.app',
+]
 
 
 # --------------------------------------------------
@@ -36,6 +38,8 @@ INSTALLED_APPS = [
 # --------------------------------------------------
 
 MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -135,7 +139,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # --------------------------------------------------
 # ARCHIVOS MEDIA (PDFS Y FIRMAS)
